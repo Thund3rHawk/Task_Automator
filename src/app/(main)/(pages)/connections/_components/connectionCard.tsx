7e0a54471b5217f562ff43f889df1deecb52a929
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-07-21 01:20:39 +0530

    refactor: connections card

--- src/app/(main)/(pages)/connections/_components/connectionCard.tsx
+++ src/app/(main)/(pages)/connections/_components/connectionCard.tsx
@@ -1,27 +1,56 @@
-import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import Image from 'next/image'
-import React from 'react'
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 type Props = {
-    icon: string,
-    title: string,
-    description: string
-}
+  icon: string;
+  title: string;
+  description: string;
+  type: string;
+};
 
-const ConnectionCard = ({icon, title, description}: Props) => {
+const ConnectionCard = ({ icon, title, description }: Props) => {
   return (
-    <Card className='mx-10'>
-        <CardHeader className='flex flex-col gap-4'>
-            <div className="flex flex-row gap-2">
-                <Image src = {icon} alt = {title} height = {30} width = {30} className = 'object-contain'/>                
-            </div>
-            <div>
-                <CardTitle className='text-lg'>{title}</CardTitle>
-                <CardDescription>{description}</CardDescription>
-            </div>
-        </CardHeader>
+    <Card className="mx-10 h-28 flex items-center">
+      <CardHeader className="flex flex-row items-center w-full gap-4">
+        <div className="flex items-center justify-center mr-4">
+          <Image
+            src={icon}
+            alt={title}
+            height={30}
+            width={30}
+            className="object-contain"
+          />
+        </div>
+        <div className="flex flex-col flex-1">
+          <CardTitle className="text-lg">{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </div>
+        <div>
+          <Link
+            href={
+              title === "Discord"
+                ? process.env.NEXT_PUBLIC_DISCORD_REDIRECT || "#"
+                : title === "Notion"
+                ? process.env.NEXT_PUBLIC_NOTION_REDIRECT || "#"
+                : title === "Slack"
+                ? process.env.NEXT_PUBLIC_SLACK_REDIRECT || "#"
+                : "#"
+            }
+            className="rounded-lg bg-primary p-2 font-bold text-primary-foreground"
+          >
+            Connect
+          </Link>
+        </div>
+      </CardHeader>
     </Card>
-  )
-}
+  );
+};
 
-export default ConnectionCard
+export default ConnectionCard;
--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -1,27 +1,35 @@
-import { CONNECTIONS } from '@/lib/constant'
-import React from 'react'
-import ConnectionCard from './_components/connectionCard'
+import { CONNECTIONS } from "@/lib/constant";
+import React from "react";
+import ConnectionCard from "./_components/connectionCard";
 
-type Props = {}
+type Props = {};
 
 const Page = (props: Props) => {
   return (
-    <div className='relative flex flex-col gap-4'> 
+    <div className="relative flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
         Connections
       </h1>
       <div className="relative flex flex-col gap-4">
         <section className="flex flex-col gap-4 p-6 text-muted-foreground ">
-          Connect all apps directly from here. You may need to connect these apps regularly to refresh verification
+          Connect all apps directly from here. You may need to connect these
+          apps regularly to refresh verification
         </section>
-        {CONNECTIONS.map((item, index)=>{
+        {CONNECTIONS.map((item, index) => {
           return (
-            <ConnectionCard key = {index} icon={item.image} title= {item.title} description = {item.description} />
-          )
+            <ConnectionCard
+              key={index}
+              icon={item.image}
+              title={item.title}
+              description={item.description}
+              type = {item.title}
+              // connected = {connections}
+            />
+          );
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
