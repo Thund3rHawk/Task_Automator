fa2b7d66becf83ce4db7c50a994ecba111750d16
thund3rhawk <2025.it18.swadhinpaul@gmail.com>
2025-07-23 12:52:00 +0530

    refactor: update lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,14 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
       "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-empty-object-type": "warn"
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "@typescript-eslint/no-require-imports": "warn",
+      "@typescript-eslint/no-this-alias": "warn",
+      "@typescript-eslint/no-wrapper-object-types": "warn",
+      "@typescript-eslint/no-unnecessary-type-constraint": "warn",
+      "@typescript-eslint/no-unsafe-function-type": "warn",
+
     }
   }
 ];
